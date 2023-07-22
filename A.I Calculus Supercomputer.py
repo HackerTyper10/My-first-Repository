80d84e6ee179b206ef1c2f2792669211e7069f08
Chai_Tea <139700812+HackerTyper10@users.noreply.github.com>
2023-07-22 09:16:00 -0500

    Create A.I Calculus Supercomputer.py
    A.i Supercomputer/calculator that knows calculus, pre calculus, algebra, pre algebra, and math.

---
+++ A.I Calculus Supercomputer.py
@@ -0,0 +1,19 @@
+import time
+print("Hello")
+print("I am a. A.I Math, Pre algebra, Algebra, Geometry, Algebra 2, and Calculus supercomputer. Just feed your question through the input and I will have your answer shortly.")
+q = input("> ")
+print("...")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("I have your answer!")
+time.sleep(0.5)
+print("Try studying, you cheating monster. I have already compiled your teacher's number and I am calling them to report you right now.")
+
+
+
+
+
+
+
+
